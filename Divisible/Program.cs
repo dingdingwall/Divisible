a5cd027363d7b4caa25a250d101e07c19cb7e50a
dingdingwall <johndalepardillo@gmail.com>
2023-03-12 21:52:02 +0800

    Add project files.

---
+++ Divisible/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Divisibleby
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an interger");
+
+            double num = double.Parse(Console.ReadLine());
+
+            if(num > 0 && num % 11 ==0 ) 
+            {
+
+
+                Console.WriteLine($"{num} is divisible by 11.");
+            }
+            else
+            {
+                Console.WriteLine($"{num} is not divisible by 11.");
+            }
+        }
+    }
+}
